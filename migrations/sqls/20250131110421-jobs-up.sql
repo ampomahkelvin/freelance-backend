f3dd9e05505323b626217ea505c5a3b2eb5ff870
ampomahkelvin <kevboat@gmail.com>
2025-02-07 18:45:58 +0000

    initial commit

--- migrations/sqls/20250131110421-jobs-up.sql
+++ migrations/sqls/20250131110421-jobs-up.sql
@@ -1,5 +1,6 @@
 /* Replace with your SQL commands */
 CREATE TYPE "budget_type" AS ENUM('hourly', 'fixed');
+CREATE TYPE "job_status" AS ENUM('opened', 'accepted', 'declined', 'closed', 'completed');
 
 CREATE TABLE IF NOT EXISTS "jobs" (
     "job_id" SERIAL PRIMARY KEY UNIQUE NOT NULL,
@@ -8,7 +9,10 @@ CREATE TABLE IF NOT EXISTS "jobs" (
     "description" TEXT,
     "budget_type" budget_type DEFAULT 'fixed',
     "budget" DECIMAL,
-    "status" VARCHAR NOT NULL,
+    "status" job_status NOT NULL,
     "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     "updated_at" TIMESTAMPTZ NOT NULL DEFAULT NOW()
-)
+);
+
+CREATE INDEX idx_jobs_client_id ON "jobs" ("client_id");
+CREATE INDEX idx_jobs_status ON "jobs" ("status");
--- migrations/sqls/20250131110441-applications-up.sql
+++ migrations/sqls/20250131110441-applications-up.sql
@@ -5,6 +5,7 @@ CREATE TABLE IF NOT EXISTS "applications" (
     "application_id" SERIAL PRIMARY KEY UNIQUE NOT NULL,
     "job_id" INTEGER NOT NULL,
     "freelancer_id" INTEGER NOT NULL,
-    "status" application_status NOT NULL,
-    "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW()
+    "status" application_status DEFAULT 'pending',
+    "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    "updated_at" TIMESTAMPTZ NOT NULL DEFAULT NOW()
 )
